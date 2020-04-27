e2211f35e7fb63e67bb31160ef04c47ae26fbf2f
notapassword <whyisntthisavalid@gmail.com>
2020-04-27 16:28:40 -0400

    updated

---
+++ beaqlejs_2/config/example_config_preference.js
@@ -0,0 +1,31 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Preference Demo Test",
+  "LoopByDefault": false,
+  "ShowFileIDs": false,
+  "ShowResults": true,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": false,
+  "BeaqleServiceURL": "/web_service/beaqleJS_Service.php",
+  "SupervisorContact": "",
+  "AudioRoot": "",
+  "Testsets": [
+    //
+    {
+      "Name": "",
+      "TestID": "a0129",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0129.wav",
+            "B": "audio/standardlab_arctic_a0129.wav",
+      }
+    },
+    {
+      "Name": "",
+      "TestID": "a0188",
+      "Files": {
+            "A": "audio/standardehmm_arctic_a0188.wav",
+            "B": "audio/standardlab_arctic_a0188.wav",
+      }
+    },
+  ]
+}
