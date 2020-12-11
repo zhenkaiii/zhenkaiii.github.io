304dd3d8fdb6caee9960ed4ddc19ba45b6753afb
notapassword <whyisntthisavalid@gmail.com>
2020-12-10 19:35:44 -0500

    updated

---
+++ beaqlejs/config/ablation_blending_weights.js
@@ -0,0 +1,124 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Mushra Test",
+  "RateScalePng": "img/scale_abs.png",
+  "RateScaleBgPng": "img/scale_abs_background.png",
+  "RateMinValue": 0,
+  "RateMaxValue": 100,
+  "RateDefaultValue":100,
+  "ShowFileIDs": false,
+  "ShowResults": false,
+  "LoopByDefault": true,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": true,
+  "BeaqleServiceURL": "beaqleJS_Service.php",
+  "SupervisorContact": "zhenk@iu.edu",
+  "RandomizeTestOrder": false,
+  "MaxTestsPerRun": 10,
+  "RequireMaxRating": false,
+  "AudioRoot": "taslp_samples/",
+  "Testsets": [
+    //
+    {
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MRWS1-SX410.wav" ,
+        "2":"/k/DR5-MRWS1-SX410.wav",
+        "3":"/6-i/DR5-MRWS1-SX410.wav",
+        "4":"/6-ii/DR5-MRWS1-SX410.wav",
+        "5":"/6-iii/DR5-MRWS1-SX410.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR3-FKMS0-SX230.wav" ,
+        "2":"/k/DR3-FKMS0-SX230.wav",
+        "3":"/6-i/DR3-FKMS0-SX230.wav",
+        "4":"/6-ii/DR3-FKMS0-SX230.wav",
+        "5":"/6-iii/DR3-FKMS0-SX230.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-MRJM4-SI859.wav" ,
+        "2":"/k/DR7-MRJM4-SI859.wav",
+        "3":"/6-i/DR7-MRJM4-SI859.wav",
+        "4":"/6-ii/DR7-MRJM4-SI859.wav",
+        "5":"/6-iii/DR7-MRJM4-SI859.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MBPM0-SA1.wav" ,
+        "2":"/k/DR5-MBPM0-SA1.wav",
+        "3":"/6-i/DR5-MBPM0-SA1.wav",
+        "4":"/6-ii/DR5-MBPM0-SA1.wav",
+        "5":"/6-iii/DR5-MBPM0-SA1.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR2-FJWB0-SX95.wav" ,
+        "2":"/k/DR2-FJWB0-SX95.wav",
+        "3":"/6-i/DR2-FJWB0-SX95.wav",
+        "4":"/6-ii/DR2-FJWB0-SX95.wav",
+        "5":"/6-iii/DR2-FJWB0-SX95.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR8-FJSJ0-SX44.wav" ,
+        "2":"/k/DR8-FJSJ0-SX44.wav",
+        "3":"/6-i/DR8-FJSJ0-SX44.wav",
+        "4":"/6-ii/DR8-FJSJ0-SX44.wav",
+        "5":"/6-iii/DR8-FJSJ0-SX44.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR6-FMGD0-SX34.wav" ,
+        "2":"/k/DR6-FMGD0-SX34.wav",
+        "3":"/6-i/DR6-FMGD0-SX34.wav",
+        "4":"/6-ii/DR6-FMGD0-SX34.wav",
+        "5":"/6-iii/DR6-FMGD0-SX34.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR1-MREB0-SA2.wav" ,
+        "2":"/k/DR1-MREB0-SA2.wav",
+        "3":"/6-i/DR1-MREB0-SA2.wav",
+        "4":"/6-ii/DR1-MREB0-SA2.wav",
+        "5":"/6-iii/DR1-MREB0-SA2.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-FTLH0-SA2.wav" ,
+        "2":"/k/DR7-FTLH0-SA2.wav",
+        "3":"/6-i/DR7-FTLH0-SA2.wav",
+        "4":"/6-ii/DR7-FTLH0-SA2.wav",
+        "5":"/6-iii/DR7-FTLH0-SA2.wav",
+    }
+},{
+  "Name": "Session-II (Mid Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR4-MBNS0-SI1850.wav" ,
+        "2":"/k/DR4-MBNS0-SI1850.wav",
+        "3":"/6-i/DR4-MBNS0-SI1850.wav",
+        "4":"/6-ii/DR4-MBNS0-SI1850.wav",
+        "5":"/6-iii/DR4-MBNS0-SI1850.wav",
+    }
+},
+  ]
+}
---
+++ beaqlejs/config/taslp_mushra_high.js
@@ -0,0 +1,124 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Mushra Test",
+  "RateScalePng": "img/scale_abs.png",
+  "RateScaleBgPng": "img/scale_abs_background.png",
+  "RateMinValue": 0,
+  "RateMaxValue": 100,
+  "RateDefaultValue":100,
+  "ShowFileIDs": true,
+  "ShowResults": false,
+  "LoopByDefault": true,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": true,
+  "BeaqleServiceURL": "beaqleJS_Service.php",
+  "SupervisorContact": "zhenk@iu.edu",
+  "RandomizeTestOrder": false,
+  "MaxTestsPerRun": 10,
+  "RequireMaxRating": false,
+  "AudioRoot": "taslp_samples/",
+  "Testsets": [
+    //
+    {
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MRWS1-SX410.wav" ,
+        "2":"/k/DR5-MRWS1-SX410.wav",
+        "3":"/a/DR5-MRWS1-SX410.wav",
+        "4":"/g/DR5-MRWS1-SX410.wav",
+        "5":"/h/DR5-MRWS1-SX410.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR3-FKMS0-SX230.wav" ,
+        "2":"/k/DR3-FKMS0-SX230.wav",
+        "3":"/a/DR3-FKMS0-SX230.wav",
+        "4":"/g/DR3-FKMS0-SX230.wav",
+        "5":"/h/DR3-FKMS0-SX230.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-MRJM4-SI859.wav" ,
+        "2":"/k/DR7-MRJM4-SI859.wav",
+        "3":"/a/DR7-MRJM4-SI859.wav",
+        "4":"/g/DR7-MRJM4-SI859.wav",
+        "5":"/h/DR7-MRJM4-SI859.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MBPM0-SA1.wav" ,
+        "2":"/k/DR5-MBPM0-SA1.wav",
+        "3":"/a/DR5-MBPM0-SA1.wav",
+        "4":"/g/DR5-MBPM0-SA1.wav",
+        "5":"/h/DR5-MBPM0-SA1.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR2-FJWB0-SX95.wav" ,
+        "2":"/k/DR2-FJWB0-SX95.wav",
+        "3":"/a/DR2-FJWB0-SX95.wav",
+        "4":"/g/DR2-FJWB0-SX95.wav",
+        "5":"/h/DR2-FJWB0-SX95.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR8-FJSJ0-SX44.wav" ,
+        "2":"/k/DR8-FJSJ0-SX44.wav",
+        "3":"/a/DR8-FJSJ0-SX44.wav",
+        "4":"/g/DR8-FJSJ0-SX44.wav",
+        "5":"/h/DR8-FJSJ0-SX44.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR6-FMGD0-SX34.wav" ,
+        "2":"/k/DR6-FMGD0-SX34.wav",
+        "3":"/a/DR6-FMGD0-SX34.wav",
+        "4":"/g/DR6-FMGD0-SX34.wav",
+        "5":"/h/DR6-FMGD0-SX34.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR1-MREB0-SA2.wav" ,
+        "2":"/k/DR1-MREB0-SA2.wav",
+        "3":"/a/DR1-MREB0-SA2.wav",
+        "4":"/g/DR1-MREB0-SA2.wav",
+        "5":"/h/DR1-MREB0-SA2.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-FTLH0-SA2.wav" ,
+        "2":"/k/DR7-FTLH0-SA2.wav",
+        "3":"/a/DR7-FTLH0-SA2.wav",
+        "4":"/g/DR7-FTLH0-SA2.wav",
+        "5":"/h/DR7-FTLH0-SA2.wav",
+    }
+},{
+  "Name": "Session-III (High Bitrates)",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR4-MBNS0-SI1850.wav" ,
+        "2":"/k/DR4-MBNS0-SI1850.wav",
+        "3":"/a/DR4-MBNS0-SI1850.wav",
+        "4":"/g/DR4-MBNS0-SI1850.wav",
+        "5":"/h/DR4-MBNS0-SI1850.wav",
+    }
+},
+  ]
+}
