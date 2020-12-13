7aeb9e1162b468090171571b54c26986ac8643aa
notapassword <whyisntthisavalid@gmail.com>
2020-12-13 12:49:33 -0500

    updated

--- beaqlejs/config/taslp_mushra_low_i.js
+++ beaqlejs/config/taslp_mushra_low_i.js
@@ -25,7 +25,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR5-MRWS1-SX410.wav" ,
         "2":"/k/DR5-MRWS1-SX410.wav",
-        "3":"/1-i/DR5-MRWS1-SX410.wav",
+        "3":"/1/DR5-MRWS1-SX410.wav",
         "4":"/6/DR5-MRWS1-SX410.wav",
         "5":"/7/DR5-MRWS1-SX410.wav",
     }
@@ -35,7 +35,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR3-FKMS0-SX230.wav" ,
         "2":"/k/DR3-FKMS0-SX230.wav",
-        "3":"/1-i/DR3-FKMS0-SX230.wav",
+        "3":"/1/DR3-FKMS0-SX230.wav",
         "4":"/6/DR3-FKMS0-SX230.wav",
         "5":"/7/DR3-FKMS0-SX230.wav",
     }
@@ -45,7 +45,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR7-MRJM4-SI859.wav" ,
         "2":"/k/DR7-MRJM4-SI859.wav",
-        "3":"/1-i/DR7-MRJM4-SI859.wav",
+        "3":"/1/DR7-MRJM4-SI859.wav",
         "4":"/6/DR7-MRJM4-SI859.wav",
         "5":"/7/DR7-MRJM4-SI859.wav",
     }
@@ -55,7 +55,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR5-MBPM0-SA1.wav" ,
         "2":"/k/DR5-MBPM0-SA1.wav",
-        "3":"/1-i/DR5-MBPM0-SA1.wav",
+        "3":"/1/DR5-MBPM0-SA1.wav",
         "4":"/6/DR5-MBPM0-SA1.wav",
         "5":"/7/DR5-MBPM0-SA1.wav",
     }
@@ -65,7 +65,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR2-FJWB0-SX95.wav" ,
         "2":"/k/DR2-FJWB0-SX95.wav",
-        "3":"/1-i/DR2-FJWB0-SX95.wav",
+        "3":"/1/DR2-FJWB0-SX95.wav",
         "4":"/6/DR2-FJWB0-SX95.wav",
         "5":"/7/DR2-FJWB0-SX95.wav",
     }
@@ -75,7 +75,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR8-FJSJ0-SX44.wav" ,
         "2":"/k/DR8-FJSJ0-SX44.wav",
-        "3":"/1-i/DR8-FJSJ0-SX44.wav",
+        "3":"/1/DR8-FJSJ0-SX44.wav",
         "4":"/6/DR8-FJSJ0-SX44.wav",
         "5":"/7/DR8-FJSJ0-SX44.wav",
     }
@@ -85,7 +85,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR6-FMGD0-SX34.wav" ,
         "2":"/k/DR6-FMGD0-SX34.wav",
-        "3":"/1-i/DR6-FMGD0-SX34.wav",
+        "3":"/1/DR6-FMGD0-SX34.wav",
         "4":"/6/DR6-FMGD0-SX34.wav",
         "5":"/7/DR6-FMGD0-SX34.wav",
     }
@@ -95,7 +95,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR1-MREB0-SA2.wav" ,
         "2":"/k/DR1-MREB0-SA2.wav",
-        "3":"/1-i/DR1-MREB0-SA2.wav",
+        "3":"/1/DR1-MREB0-SA2.wav",
         "4":"/6/DR1-MREB0-SA2.wav",
         "5":"/7/DR1-MREB0-SA2.wav",
     }
@@ -105,7 +105,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR7-FTLH0-SA2.wav" ,
         "2":"/k/DR7-FTLH0-SA2.wav",
-        "3":"/1-i/DR7-FTLH0-SA2.wav",
+        "3":"/1/DR7-FTLH0-SA2.wav",
         "4":"/6/DR7-FTLH0-SA2.wav",
         "5":"/7/DR7-FTLH0-SA2.wav",
     }
@@ -115,7 +115,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR4-MBNS0-SI1850.wav" ,
         "2":"/k/DR4-MBNS0-SI1850.wav",
-        "3":"/1-i/DR4-MBNS0-SI1850.wav",
+        "3":"/1/DR4-MBNS0-SI1850.wav",
         "4":"/6/DR4-MBNS0-SI1850.wav",
         "5":"/7/DR4-MBNS0-SI1850.wav",
     }
--- beaqlejs/config/taslp_mushra_low_ii.js
+++ beaqlejs/config/taslp_mushra_low_ii.js
@@ -25,7 +25,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR5-MRWS1-SX410.wav" ,
         "2":"/k/DR5-MRWS1-SX410.wav",
-        "3":"/1-i/DR5-MRWS1-SX410.wav",
+        "3":"/1/DR5-MRWS1-SX410.wav",
         "4":"/b/DR5-MRWS1-SX410.wav",
         "5":"/e/DR5-MRWS1-SX410.wav",
     }
@@ -35,7 +35,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR3-FKMS0-SX230.wav" ,
         "2":"/k/DR3-FKMS0-SX230.wav",
-        "3":"/1-i/DR3-FKMS0-SX230.wav",
+        "3":"/1/DR3-FKMS0-SX230.wav",
         "4":"/b/DR3-FKMS0-SX230.wav",
         "5":"/e/DR3-FKMS0-SX230.wav",
     }
@@ -45,7 +45,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR7-MRJM4-SI859.wav" ,
         "2":"/k/DR7-MRJM4-SI859.wav",
-        "3":"/1-i/DR7-MRJM4-SI859.wav",
+        "3":"/1/DR7-MRJM4-SI859.wav",
         "4":"/b/DR7-MRJM4-SI859.wav",
         "5":"/e/DR7-MRJM4-SI859.wav",
     }
@@ -55,7 +55,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR5-MBPM0-SA1.wav" ,
         "2":"/k/DR5-MBPM0-SA1.wav",
-        "3":"/1-i/DR5-MBPM0-SA1.wav",
+        "3":"/1/DR5-MBPM0-SA1.wav",
         "4":"/b/DR5-MBPM0-SA1.wav",
         "5":"/e/DR5-MBPM0-SA1.wav",
     }
@@ -65,7 +65,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR2-FJWB0-SX95.wav" ,
         "2":"/k/DR2-FJWB0-SX95.wav",
-        "3":"/1-i/DR2-FJWB0-SX95.wav",
+        "3":"/1/DR2-FJWB0-SX95.wav",
         "4":"/b/DR2-FJWB0-SX95.wav",
         "5":"/e/DR2-FJWB0-SX95.wav",
     }
@@ -75,7 +75,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR8-FJSJ0-SX44.wav" ,
         "2":"/k/DR8-FJSJ0-SX44.wav",
-        "3":"/1-i/DR8-FJSJ0-SX44.wav",
+        "3":"/1/DR8-FJSJ0-SX44.wav",
         "4":"/b/DR8-FJSJ0-SX44.wav",
         "5":"/e/DR8-FJSJ0-SX44.wav",
     }
@@ -85,7 +85,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR6-FMGD0-SX34.wav" ,
         "2":"/k/DR6-FMGD0-SX34.wav",
-        "3":"/1-i/DR6-FMGD0-SX34.wav",
+        "3":"/1/DR6-FMGD0-SX34.wav",
         "4":"/b/DR6-FMGD0-SX34.wav",
         "5":"/e/DR6-FMGD0-SX34.wav",
     }
@@ -95,7 +95,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR1-MREB0-SA2.wav" ,
         "2":"/k/DR1-MREB0-SA2.wav",
-        "3":"/1-i/DR1-MREB0-SA2.wav",
+        "3":"/1/DR1-MREB0-SA2.wav",
         "4":"/b/DR1-MREB0-SA2.wav",
         "5":"/e/DR1-MREB0-SA2.wav",
     }
@@ -105,7 +105,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR7-FTLH0-SA2.wav" ,
         "2":"/k/DR7-FTLH0-SA2.wav",
-        "3":"/1-i/DR7-FTLH0-SA2.wav",
+        "3":"/1/DR7-FTLH0-SA2.wav",
         "4":"/b/DR7-FTLH0-SA2.wav",
         "5":"/e/DR7-FTLH0-SA2.wav",
     }
@@ -115,7 +115,7 @@ var TestConfig = {
   "Files": {
         "Reference":"/d/DR4-MBNS0-SI1850.wav" ,
         "2":"/k/DR4-MBNS0-SI1850.wav",
-        "3":"/1-i/DR4-MBNS0-SI1850.wav",
+        "3":"/1/DR4-MBNS0-SI1850.wav",
         "4":"/b/DR4-MBNS0-SI1850.wav",
         "5":"/e/DR4-MBNS0-SI1850.wav",
     }
