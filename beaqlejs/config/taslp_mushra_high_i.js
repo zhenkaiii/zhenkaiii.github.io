d608636d6cfe1a2b00fcd1b42fae311fb282c443
notapassword <whyisntthisavalid@gmail.com>
2020-12-14 12:17:56 -0500

    highrates

---
+++ beaqlejs/config/taslp_mushra_high_i.js
@@ -0,0 +1,134 @@
+// configure the test here
+var TestConfig = {
+  "TestName": "Mushra Test",
+  "RateScalePng": "img/scale_abs.png",
+  "RateScaleBgPng": "img/scale_abs_background.png",
+  "RateMinValue": 0,
+  "RateMaxValue": 100,
+  "RateDefaultValue":100,
+  "ShowFileIDs": false,
+  "ShowResults": false,
+  "LoopByDefault": true,
+  "EnableABLoop": true,
+  "EnableOnlineSubmission": true,
+  "BeaqleServiceURL": "beaqleJS_Service.php",
+  "SupervisorContact": "zhenk@iu.edu",
+  "RandomizeTestOrder": false,
+  "MaxTestsPerRun": 10,
+  "RequireMaxRating": false,
+  "AudioRoot": "taslp_samples/",
+  "Testsets": [
+    //
+    {
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MRWS1-SX410.wav" ,
+        "2":"/k/DR5-MRWS1-SX410.wav",
+        "3":"/9/DR5-MRWS1-SX410.wav",
+        "4":"/4/DR5-MRWS1-SX410.wav",
+        "5":"/3/DR5-MRWS1-SX410.wav",
+        "6":"/a/DR5-MRWS1-SX410.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR3-FKMS0-SX230.wav" ,
+        "2":"/k/DR3-FKMS0-SX230.wav",
+        "3":"/9/DR3-FKMS0-SX230.wav",
+        "4":"/4/DR3-FKMS0-SX230.wav",
+        "5":"/3/DR3-FKMS0-SX230.wav",
+        "6":"/a/DR3-FKMS0-SX230.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-MRJM4-SI859.wav" ,
+        "2":"/k/DR7-MRJM4-SI859.wav",
+        "3":"/9/DR7-MRJM4-SI859.wav",
+        "4":"/4/DR7-MRJM4-SI859.wav",
+        "5":"/3/DR7-MRJM4-SI859.wav",
+        "6":"/a/DR7-MRJM4-SI859.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR5-MBPM0-SA1.wav" ,
+        "2":"/k/DR5-MBPM0-SA1.wav",
+        "3":"/9/DR5-MBPM0-SA1.wav",
+        "4":"/4/DR5-MBPM0-SA1.wav",
+        "5":"/3/DR5-MBPM0-SA1.wav",
+        "6":"/a/DR5-MBPM0-SA1.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR2-FJWB0-SX95.wav" ,
+        "2":"/k/DR2-FJWB0-SX95.wav",
+        "3":"/9/DR2-FJWB0-SX95.wav",
+        "4":"/4/DR2-FJWB0-SX95.wav",
+        "5":"/3/DR2-FJWB0-SX95.wav",
+        "6":"/a/DR2-FJWB0-SX95.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR8-FJSJ0-SX44.wav" ,
+        "2":"/k/DR8-FJSJ0-SX44.wav",
+        "3":"/9/DR8-FJSJ0-SX44.wav",
+        "4":"/4/DR8-FJSJ0-SX44.wav",
+        "5":"/3/DR8-FJSJ0-SX44.wav",
+        "6":"/a/DR8-FJSJ0-SX44.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR6-FMGD0-SX34.wav" ,
+        "2":"/k/DR6-FMGD0-SX34.wav",
+        "3":"/9/DR6-FMGD0-SX34.wav",
+        "4":"/4/DR6-FMGD0-SX34.wav",
+        "5":"/3/DR6-FMGD0-SX34.wav",
+        "6":"/a/DR6-FMGD0-SX34.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR1-MREB0-SA2.wav" ,
+        "2":"/k/DR1-MREB0-SA2.wav",
+        "3":"/9/DR1-MREB0-SA2.wav",
+        "4":"/4/DR1-MREB0-SA2.wav",
+        "5":"/3/DR1-MREB0-SA2.wav",
+        "6":"/a/DR1-MREB0-SA2.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR7-FTLH0-SA2.wav" ,
+        "2":"/k/DR7-FTLH0-SA2.wav",
+        "3":"/9/DR7-FTLH0-SA2.wav",
+        "4":"/4/DR7-FTLH0-SA2.wav",
+        "5":"/3/DR7-FTLH0-SA2.wav",
+        "6":"/a/DR7-FTLH0-SA2.wav",
+    }
+},{
+  "Name": "Session-III-A",
+  "TestID": "id1_1",
+  "Files": {
+        "Reference":"/d/DR4-MBNS0-SI1850.wav" ,
+        "2":"/k/DR4-MBNS0-SI1850.wav",
+        "3":"/9/DR4-MBNS0-SI1850.wav",
+        "4":"/4/DR4-MBNS0-SI1850.wav",
+        "5":"/3/DR4-MBNS0-SI1850.wav",
+        "6":"/a/DR4-MBNS0-SI1850.wav",
+    }
+},
+  ]
+}
